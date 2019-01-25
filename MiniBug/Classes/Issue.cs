7ea2234aee56e5872eb0264bb4ee2d507dab7b05
Joao Martiniano <joao.martiniano@gmail.com>
2019-01-25 22:54:02 +0000

    Add Issue class

---
+++ MiniBug/Classes/Issue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBug
+{
+    public enum IssueStatus { None = 0, Unconfirmed, Confirmed, InProgress, Resolved, Verified };
+
+    public enum IssuePriority { None = 0, Low, Normal, High, Urgent, Immediate };
+
+    class Issue
+    {
+        /// <summary>
+        /// Gets the ID of this issue.
+        /// </summary>
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the status of this issue.
+        /// </summary>
+        public IssueStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the priority of this issue.
+        /// </summary>
+        public IssuePriority Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary of this issue.
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of this issue.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of the software on which this issue occurs.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the future version on which this issue will be resolved.
+        /// </summary>
+        public string TargetVersion { get; set; }
+
+        /// <summary>
+        /// Gets the date/time this issue was created.
+        /// </summary>
+        public DateTime DateCreated { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the date/time this issue was modified.
+        /// </summary>
+        public DateTime DateModified { get; set; }
+
+    }
+}
