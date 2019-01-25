9a4761c9368f352752b37beff55bb9f0674a98dc
Joao Martiniano <joao.martiniano@gmail.com>
2019-01-25 23:10:46 +0000

    Add Task class

---
+++ MiniBug/Classes/Task.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBug.Classes
+{
+    public enum TaskStatus { None = 0 /* ....add more */ };
+
+    public enum TaskPriority { None = 0, Low, Normal, High, Urgent, Immediate };
+
+    class Task
+    {
+        /// <summary>
+        /// Gets the ID of this task.
+        /// </summary>
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the status of this task.
+        /// </summary>
+        public TaskStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the priority of this task.
+        /// </summary>
+        public TaskPriority Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary of this task.
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of this task.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the future version on which this task will be applied/finished.
+        /// </summary>
+        public string TargetVersion { get; set; }
+
+        /// <summary>
+        /// Gets the date/time this task was created.
+        /// </summary>
+        public DateTime DateCreated { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the date/time this task was modified.
+        /// </summary>
+        public DateTime DateModified { get; set; }
+
+
+    }
+}
