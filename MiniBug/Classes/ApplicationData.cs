9a8a6f863f9e59b76ede0c0211ea61ec6ef666f8
Joao Martiniano <joao.martiniano@gmail.com>
2019-01-26 11:44:10 +0000

    Start using JavaScriptSerializer class
    Added a reference to System.Web.Extensions to be able to use the JavaScriptSerializer class. First approach to loading and saving the application data.

--- MiniBug/Classes/ApplicationData.cs
+++ MiniBug/Classes/ApplicationData.cs
@@ -26,69 +26,69 @@ namespace MiniBug
         /// </summary>
         public static List<Task> Tasks { get; set; } = new List<Task>();
 
-        ///// <summary>
-        ///// Load application data.
-        ///// </summary>
-        //public static FileOperationsStatus Load()
-        //{
-        //    String json = string.Empty;
+        /// <summary>
+        /// Load application data.
+        /// </summary>
+        public static FileOperationsStatus Load()
+        {
+            String json = string.Empty;
 
-        //    try
-        //    {
-        //        // Open the file
-        //        System.IO.StreamReader r = new System.IO.StreamReader(Filename);
+            try
+            {
+                // Open the file
+                System.IO.StreamReader r = new System.IO.StreamReader(Filename);
 
-        //        // Read the file contents
-        //        json = r.ReadToEnd();
+                // Read the file contents
+                json = r.ReadToEnd();
 
-        //        r.Close();
-        //    }
-        //    catch (System.IO.DirectoryNotFoundException) // The directory does not exist
-        //    {
-        //        return FileOperationsStatus.DirectoryNotFound;
-        //    }
-        //    catch (System.IO.FileNotFoundException) // The file does not exist
-        //    {
-        //        return FileOperationsStatus.FileNotFound;
-        //    }
-        //    catch
-        //    {
-        //        return FileOperationsStatus.IOError;
-        //    }
+                r.Close();
+            }
+            catch (System.IO.DirectoryNotFoundException) // The directory does not exist
+            {
+                return FileOperationsStatus.DirectoryNotFound;
+            }
+            catch (System.IO.FileNotFoundException) // The file does not exist
+            {
+                return FileOperationsStatus.FileNotFound;
+            }
+            catch
+            {
+                return FileOperationsStatus.IOError;
+            }
 
-        //    //Categories = new JavaScriptSerializer().Deserialize<List<Category>>(json);
+            //Categories = new JavaScriptSerializer().Deserialize<List<Category>>(json);
 
-        //    return FileOperationsStatus.Success;
-        //}
+            return FileOperationsStatus.Success;
+        }
 
-        ///// <summary>
-        ///// Saves the application data. The file is overwritten.
-        ///// </summary>
-        //public static FileOperationsStatus Save()
-        //{
-        //    var serializer = new JavaScriptSerializer();
-        //    var serializedResult = serializer.Serialize(Categories);
+        /// <summary>
+        /// Saves the application data. The file is overwritten.
+        /// </summary>
+        public static FileOperationsStatus Save()
+        {
+            var serializer = new JavaScriptSerializer();
+            var serializedResult = serializer.Serialize(Issues);
 
-        //    try
-        //    {
-        //        System.IO.File.WriteAllText(Filename, serializedResult);
-        //    }
-        //    catch (System.IO.DirectoryNotFoundException) // The directory does not exist
-        //    {
-        //        return FileOperationsStatus.DirectoryNotFound;
-        //    }
-        //    catch (System.IO.PathTooLongException) // The path is too long
-        //    {
-        //        return FileOperationsStatus.PathTooLong;
-        //    }
-        //    catch
-        //    {
-        //        return FileOperationsStatus.IOError;
-        //    }
+            serializedResult += serializer.Serialize(Tasks);
 
-        //    return FileOperationsStatus.Success;
-        //}
+            try
+            {
+                System.IO.File.WriteAllText(Filename, serializedResult);
+            }
+            catch (System.IO.DirectoryNotFoundException) // The directory does not exist
+            {
+                return FileOperationsStatus.DirectoryNotFound;
+            }
+            catch (System.IO.PathTooLongException) // The path is too long
+            {
+                return FileOperationsStatus.PathTooLong;
+            }
+            catch
+            {
+                return FileOperationsStatus.IOError;
+            }
 
-
+            return FileOperationsStatus.Success;
+        }
     }
 }
