c2b0905ee298693c230e7f7d3b853b2971c77153
Joao Martiniano <joao.martiniano@gmail.com>
2019-05-09 16:34:53 +0100

    Remove form icon and do not show on taskbar

--- MiniBug/SettingsForm.Designer.cs
+++ MiniBug/SettingsForm.Designer.cs
@@ -296,6 +296,8 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SettingsForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Settings";
             this.Load += new System.EventHandler(this.SettingsForm_Load);
